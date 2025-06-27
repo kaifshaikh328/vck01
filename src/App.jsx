ccc0ddb114f88939a4e198fb5abbd15b14b41c66
kaifshaikh328 <ks047935@gmail.com>
2025-06-27 15:30:46 +0530

    forth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React,{useState} from "react";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 //Pages
 import About from "./Pages/About.jsx";
@@ -13,10 +14,26 @@ import Footer from "./Components/Footer/Footer.jsx";
 
 import './App.css'
 import ChatbotComponent from "./Components/Chaatbot/chatbotcomponent.jsx";
+import DeveloperInfoPopup from "./Components/Developerinfo/DeveloperinfoPopup.jsx";
 
 const App=()=>{
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup =()=>{
+    setShowPopup(false);
+  };
+
   return(
+    <>
     <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Mahummadkaif Shaikh"
+          studentPhotoUrl="/Screenshot 2025-06-13 165827.png" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+    </div>
       <BrowserRouter>
       <div className="main-layout">
       <Header/>
@@ -34,7 +51,8 @@ const App=()=>{
       <Footer/>
       </div>
       </BrowserRouter>
-    </div>
+    </>
+
   )
 }
 
